df6e11a44562ac93234cedca781755c8159c0115
Khemkonez <khemkones@gmail.com>
2022-12-19 14:05:22 +0700

    ui module docker - close #10

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Software Engineering 2/65
+          Software Engineering 2/65 (issue-10)
         </p>
       </header>
     </div>
